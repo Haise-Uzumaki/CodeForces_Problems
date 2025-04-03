91cac955dcf45e74ac458f9be9cdbfea1b54857d
Haise <kakonsorkar1@gmail.com>
2025-04-03 20:48:12 +0600

    .

---
+++ B. Basic Data Types/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args) {
+        int a;
+        long b;
+        char c;
+        float d;
+        double e;
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        b = scanner.nextLong();
+        c = scanner.next().charAt()
+
+    }
+}
